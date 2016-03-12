039ae65a25af2cb90a0eb1ce4b407bb4d22594f8
TomShaco <qqq083866@gmail.com>
2016-03-12 19:49:56 +0800

    Update RectangleTester.java

--- RectangleTester.java
+++ RectangleTester.java
@@ -1,6 +1,5 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
 public class RectangleTester
 {
 	public static void main(String[ ] args){
@@ -9,28 +8,25 @@ public class RectangleTester
 		}
 		catch( InputMismatchException ex){
 			System.out.println("Please enter with number");
-		
 }			
 		}
 	}
 class Rectangle{
 	Scanner scanner =new Scanner(System.in);
-	int num;	
 	double w,h,X,Y;
-   int i=1;
+        int i=1;
 	void speak(){
-		num=i;
-		System.out.println("Please enter rectangle"+num+"'s width : ");
+		System.out.println("Please enter rectangle"+i+"'s width : ");
 		w=scanner.nextInt();
-		System.out.println("Please enter rectangle"+num+"'s height : ");
+		System.out.println("Please enter rectangle"+i+"'s height : ");
 		h=scanner.nextInt();
-		System.out.println("Please enter rectangle"+num+"'s X-axis : ");
+		System.out.println("Please enter rectangle"+i+"'s X-axis : ");
 		X=scanner.nextInt();
-		System.out.println("Please enter rectangle"+num+"'s Y-axis : ");
+		System.out.println("Please enter rectangle"+i+"'s Y-axis : ");
 		Y=scanner.nextInt();
-		System.out.println("Rectangle"+num+"(width = "+w+"  height = "+h+"  X-axis = "+X+"  Y-axis = "+Y);
-		System.out.println("Rectangle"+num+"'s area is "+w*h);
-		System.out.println("Rectangle"+num+"'s perimeter is "+(w+h)*2);
+		System.out.println("Rectangle"+i+"(width = "+w+"  height = "+h+"  X-axis = "+X+"  Y-axis = "+Y);
+		System.out.println("Rectangle"+i+"'s area is "+w*h);
+		System.out.println("Rectangle"+i+"'s perimeter is "+(w+h)*2);
 		i++;
 	}
 }
