af6a66a39d8430ecee94c5de3a810c7664f57f9c
HanselHu <apologize0616@gmail.com>
2016-03-14 13:38:56 +0800

    no message

--- RectangleTester.java
+++ RectangleTester.java
@@ -1,34 +1,41 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class RectangleTester
-{
-	public static void main(String[ ] args){
-		try{
-		Rectangle.speak();
-		Rectangle.speak();
-		}
-		catch( InputMismatchException ex){
+class Rectangle {
+	// height, width, x, y?
+	// 建構子
+	// method?
+	// overridding public String toString(){}
+
+}
+
+public class RectangleTester {
+	public static void main(String[] args) {
+		try {
+			// build object
+			double w, h, X, Y;
+			Scanner scanner = new Scanner(System.in);
+			for (int i = 0; i < 2; i++) {
+
+				System.out.println("Please enter rectangle" + i + "'s width : ");
+				w = scanner.nextInt();
+				System.out.println("Please enter rectangle" + i + "'s height : ");
+				h = scanner.nextInt();
+				System.out.println("Please enter rectangle" + i + "'s X-axis : ");
+				X = scanner.nextInt();
+				System.out.println("Please enter rectangle" + i + "'s Y-axis : ");
+				Y = scanner.nextInt();
+
+				Rectangle r1 = new Rectangle(w, h, X, Y);
+				System.out.println(r1);
+				
+				//function call
+				System.out.println("Area=");
+				System.out.println("Perimeter=");
+			}
+
+		} catch (InputMismatchException ex) {
 			System.out.println("Please enter with number");
-}			
 		}
-	}
-class Rectangle{
-    static int i=1;
-	static void speak(){
-	double w,h,X,Y;   
-	    Scanner scanner =new Scanner(System.in);//有燈泡提示 但不影響結果
-		System.out.println("Please enter rectangle"+i+"'s width : ");
-		w=scanner.nextInt();
-		System.out.println("Please enter rectangle"+i+"'s height : ");
-		h=scanner.nextInt();
-		System.out.println("Please enter rectangle"+i+"'s X-axis : ");
-		X=scanner.nextInt();
-		System.out.println("Please enter rectangle"+i+"'s Y-axis : ");
-		Y=scanner.nextInt();
-		System.out.println("Rectangle"+i+"(width = "+w+"  height = "+h+"  X-axis = "+X+"  Y-axis = "+Y+")");
-		System.out.println("Rectangle"+i+"'s area is "+w*h);
-		System.out.println("Rectangle"+i+"'s perimeter is "+(w+h)*2);
-		i++;
 	}
 }
